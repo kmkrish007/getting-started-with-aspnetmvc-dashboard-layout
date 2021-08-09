64aca7c9301a22c5a57dc5c017c11e606652edde
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2021-08-09 19:08:32 +0530

    ASP.NET MVC Dashboard layout

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Hosting;
+using System.Web;
+using System;
+
+namespace DashboardLayout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class spacingModel
+        {
+            public double[] cellSpacing { get; set; }
+        }
+        public ActionResult Index()
+        {
+            spacingModel modelValue = new spacingModel();
+            modelValue.cellSpacing = new double[] { 10, 10 };
+            return View(modelValue);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
